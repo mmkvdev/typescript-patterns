b06ded9103af6cc740f907188555a7803fcfeb86
mmkvdev <madhuv299@gmail.com>
2022-05-13 18:23:41 +0530

    formatting/issues-with-strings: few formats

--- usecases/issues-with-string/add-numbers.js
+++ usecases/issues-with-string/add-numbers.js
@@ -12,12 +12,12 @@ var input2 = document.getElementById("num2");
  * the below problem can be mitigated by placing checks in our business logic
  */
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 /*
   we are sending values of the input fields to add them, which is a vialation as the input values extracted
   in this case are always strings, no matter what kind of input type of our input fields are!
 */
 button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
