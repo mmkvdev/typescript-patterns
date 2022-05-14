bc3af499ede50619e9c1798369cf0c6dc1fe4b48
mmkvdev <madhuv299@gmail.com>
2022-05-14 20:05:43 +0530

    func/addingtwonumbers: sample code to add two numbers

--- project/fundamentals/app.ts
+++ project/fundamentals/app.ts
@@ -1,1 +1,10 @@
-console.log('coding is fun....');
+function add (a: number, b: number): number {
+    return a + b;
+}
+
+const n1 = 11;
+const n2 = 4.8;
+
+const result = add(n1, n2);
+
+console.log(result);
