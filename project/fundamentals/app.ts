fb5fa6b6e5df3dbc5680e023704822e57ed91107
mmkvdev <madhuv299@gmail.com>
2022-05-16 20:45:42 +0530

    func/core-types: example that covers all core fundamental types in typescript: numbers, boolean and string

--- project/fundamentals/app.ts
+++ project/fundamentals/app.ts
@@ -1,10 +1,15 @@
-function add (a: number, b: number): number {
-    return a + b;
+function add (a: number, b: number, checker: boolean, message: string): number {
+    const result = a + b;
+    if (checker) {
+        console.log(`${message}${result}`);
+    } else {
+        return a + b;
+    }
 }
 
-const n1 = 11;
+const n1 = 11; // 11.0
 const n2 = 4.8;
+const checkSum = true;
+const customMessage = 'The sum of a and b is: ';
 
-const result = add(n1, n2);
-
-console.log(result);
+add(n1, n2, checkSum, customMessage);
