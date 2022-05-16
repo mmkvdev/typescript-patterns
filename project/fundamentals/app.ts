18979b7da97a9f7a5d39592872987812a4511630
mmkvdev <madhuv299@gmail.com>
2022-05-16 21:02:12 +0530

    func/typeassignmentandinference: sample for understanding type inference

--- project/fundamentals/app.ts
+++ project/fundamentals/app.ts
@@ -7,9 +7,13 @@ function add (a: number, b: number, checker: boolean, message: string): number {
     }
 }
 
-const n1 = 11; // 11.0
+const n1 = 11; // we don't need to assign a type to an initialized variable - type inference
 const n2 = 4.8;
 const checkSum = true;
 const customMessage = 'The sum of a and b is: ';
+
+
+let n3: string; // best practise - always assign types to uninitialised variables
+n3 = 'this is a string';
 
 add(n1, n2, checkSum, customMessage);
