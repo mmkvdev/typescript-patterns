5be196fd3dd145ca10a9d90a458598c0d9b37d59
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2022-05-20 12:13:21 +0000

    func/unknown: ts code for unknown types

---
+++ project/fundamentals/unknown/app.ts
@@ -0,0 +1,15 @@
+/**
+ * `unknown` is a type that allows a developer to be picky when we validate the type later by implementing extra validation 
+ * during assignment / or any other usage.
+ */
+
+let a: unknown;
+let b: string;
+
+a = 5;
+a = 'application';
+
+// with unknown, we have to have introduce an extra check before assigning a variable with unknown type to a variable that has a fixed type.
+if (typeof a === 'string') {
+    b = a;
+}
