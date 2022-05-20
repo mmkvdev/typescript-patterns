17bd26b70cd2e8c2e05bdb1c35717eed95444846
Madhu Kiran Varma <28980183+mmkvdev@users.noreply.github.com>
2022-05-20 09:51:51 +0000

    func/functions: ts code for functions

---
+++ project/fundamentals/functions/app.ts
@@ -0,0 +1,24 @@
+// we can assign types to functions depending on what the return value is
+
+function add (n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+console.log(add(1,2));
+
+// void type -> special type which can be used with functions that don't have a return statement ( return value )
+
+/**
+ * 
+ * @param num 
+ * @returns void
+ * undefined is a valid type in typescript, but can only be used with functions that don't explicitly return anything 
+ * or functions that don't have a return statement
+ * in above cases `void` is used as javascript inherently returns `undefined`
+ */
+
+function printResult(num: number): void {
+    console.log('outputting the number: ', num);
+}
+
+console.log(printResult(add(1,2)));
