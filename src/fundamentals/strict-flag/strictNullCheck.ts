1ce3fecf3e17ed693fbdcc68653ab13982ce1801
mmkvdev <madhuv299@gmail.com>
2022-09-24 11:02:10 +0530

    ts/strict/strictNullCheck: strictNullCheck implementation with examples

---
+++ src/fundamentals/strict-flag/strictNullCheck.ts
@@ -0,0 +1,24 @@
+// strictNullCheck
+
+// first way
+const button_1 = document.querySelector("button")!;
+
+button_1.addEventListener("click", () => {
+  console.log("callback called");
+});
+
+// second way
+const button_2 = document.querySelector("button");
+
+button_2?.addEventListener("click", () => {
+  console.log("callback called");
+});
+
+// third way
+const button_3 = document.querySelector("button");
+
+if (button_3) {
+  button_3.addEventListener("click", () => {
+    console.log("callback called");
+  });
+}
