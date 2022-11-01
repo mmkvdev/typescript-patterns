2c70e1763ca8fec9098f5681a23bb523b6f361eb
mmkvdev <madhuv299@gmail.com>
2022-11-01 20:44:27 +0530

    ts/class: inheritance with protected examples

--- dist/fundamentals/classes/class.js
+++ dist/fundamentals/classes/class.js
@@ -31,15 +31,55 @@ class ITDepartment extends Department {
         this.admins = [];
         this.holiday = holiday;
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
     addAdmins(admin) {
         this.admins.push(admin);
     }
     printAdmins() {
-        console.log(this.admins);
+        console.log('admins: ', this.admins, this.employees);
     }
 }
 const it = new ITDepartment("pop", "oct 5");
 it.describe();
-it.addEmployee("Martin");
+it.addEmployee("Bruce Lee");
 it.addEmployee("Cisco");
-it.printEmployeeInformation();
+it.printAdmins();
+class AccountingDepartment extends Department {
+    constructor(id, name, age, reports) {
+        super(name, age, id);
+        this.reports = reports;
+        this.accoutingAdmins = [];
+        this.lastReport = reports[0];
+    }
+    addReport(report) {
+        this.accoutingAdmins.push(report);
+        this.lastReport = report;
+    }
+    printReportAdmins() {
+        console.log(this.accoutingAdmins, this.reports);
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        else {
+            throw new Error('No Result Found');
+        }
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+    printReport() {
+        console.log('employees: ', this.employees);
+    }
+}
+const accounting = new AccountingDepartment('2', 'Accounting', 23, ['Indian']);
+accounting.mostRecentReport = '';
+console.log(accounting.mostRecentReport);
+accounting.addReport('Financing');
+accounting.printReport();
